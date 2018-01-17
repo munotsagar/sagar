3b0b925f58c03cd14ee29d241d427f851868f267
IDC\Sagar.Munot <sagar.munot@inspiredelearning.com>
2018-01-17 16:53:28 +0530

    webhook commit

--- php/webhook.php
+++ php/webhook.php
@@ -1,6 +1,11 @@
 <?php
 $update_response = file_get_contents("php://input");
+$fpW = fopen('webhookNew.txt', "a+");	
+	fwrite($fpW, $update_response);	
+	fclose($fpW);
+
 $update = json_decode($update_response, true);
+
 if (isset($update["result"]["action"])) {
     $fp = fopen('sagar.txt', "a+");
 	
